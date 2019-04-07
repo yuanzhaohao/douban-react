6f66e9e77bba5f965b7fa3118260a423e36dd6bc
yuanzhaohao <yuanzhaohao@163.com>
2019-04-07 22:40:05 +0800

    feat: add loaging

--- src/entry/index.tsx
+++ src/entry/index.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Redirect } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from '../redux/index';
 
-import List from '../components/list';
 import Header from '../components/common/header';
+import List from '../components/list';
+import Detail from '../components/Detail';
 
 import './index.scss';
 
@@ -14,8 +15,9 @@ const App = () => (
     <Header />
     <div className="app-content">
       <Router>
-        <Route exact path={'/'} component={List}></Route>
-        <Route exact path={'/list'} component={List}></Route>
+        <Route exact path="/" component={List} />
+        <Route exact path="/detail/:id" component={Detail} />
+        <Route exact path="*" component={List} />
       </Router>
     </div>
   </Provider>
